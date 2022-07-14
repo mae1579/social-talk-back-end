f780c6a279d5c06658d3ec8efe5c99f3ad146da4
Mateusz Umięcki <mateuszumiecki@gmail.com>
2022-07-14 19:20:27 +0200

    added and configure types for models to mongoDB

---
+++ types/Messages/MessageEntity.ts
@@ -0,0 +1,11 @@
+export interface MessageEntity {
+
+    content: string;
+    from: object;
+    socketid: string;
+    time: string;
+    date: string;
+    to: string;
+
+
+}
---
+++ types/Users/UserEntity.ts
@@ -0,0 +1,9 @@
+export interface User {
+    name: string;
+    email: string;
+    password: string;
+    picture: string;
+    newMessages: Object;
+    status: string;
+
+}
